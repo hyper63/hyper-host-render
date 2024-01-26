b101a4df6db055fbc6983aa9289eec41262c0004
Tyler Hall <tyler@hyper.io>
2024-01-26 15:58:15 +0000

    chore: bump core, app, and adapter deps

--- app/deps.ts
+++ app/deps.ts
@@ -6,13 +6,13 @@ export { default as express } from 'npm:express@4.18.2';
 export * as jwt from 'https://deno.land/x/djwt@v2.2/mod.ts';
 
 // hyper core
-export { default as hyper } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.2.0/packages/core/mod.ts';
+export { default as hyper } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.3.2/packages/core/mod.ts';
 // hyper driving adapter
-export { default as app } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.2.0/packages/app-express/mod.ts';
+export { default as app } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.2.1/packages/app-express/mod.ts';
 
 // hyper driven adapters
-export { default as mongodb } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-mongodb/v3.1.3/mod.ts';
-export { default as redis } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-redis/v3.0.0/mod.js';
-export { default as elasticsearch } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-elasticsearch/v2.0.2/mod.js';
-export { default as minio } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-minio/v1.0.1/mod.js';
+export { default as mongodb } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-mongodb/v3.3.0/mod.ts';
+export { default as redis } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-redis/v3.2.2/mod.js';
+export { default as elasticsearch } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-elasticsearch/v2.0.3/mod.js';
+export { default as minio } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-minio/v1.0.2/mod.js';
 export { default as hooks } from 'https://raw.githubusercontent.com/hyper63/hyper-adapter-hooks/v1.0.6/mod.js';
